df30a482b33e6eba05ecee78fc4456c891f9c6ea
alfredo322 <nobrain173@gmail.com>
2016-11-21 20:35:39 -0500

    Update consulta_model.php

--- consulta_model.php
+++ consulta_model.php
@@ -5,7 +5,7 @@ class Consulta_model extends CI_Model
 	
 	public function verReg()
 	{
-		$sql=$this->db->query('select * from maestra1k');
+		$sql=$this->db->query('select * from maestra');
 		if($sql->num_rows()>0){
 			foreach ($sql->result() as $fila) :
 				$data[]=$fila;
@@ -23,11 +23,11 @@ class Consulta_model extends CI_Model
 			'Numero_celulares'=>$_POST['Numero_celulares']
 			);
 
-		$this->db->insert('maestra1k',$data);
+		$this->db->insert('maestra',$data);
 	}
 	public function editar_m()
 	{
-		$sql='select*from maestra1k where Id_estudiante=?';
+		$sql='select*from maestra where Id_estudiante=?';
 		$sql=$this->db->query($sql,$_GET["ID_ESTUDIANTE"]);
 		return $sql->result();
 	}
@@ -40,20 +40,20 @@ class Consulta_model extends CI_Model
 			'Fecha_nacimiento'=>$_POST ['Fecha_nacimiento'],
 			'Numero_celulares'=>$_POST ['Numero_celulares']
 			);
-		$this->db->where('Id_estudiante',$_POST['Id_estudiante']);
-		$this->db->update('maestra1k',$data);
+		$this->db->where('id_estudiante',$_POST['id_estudiante']);
+		$this->db->update('maestra',$data);
 	}
 	public function borrar_m()
 	{
-		$Id_estudiante=$_GET['ID_ESTUDIANTE'];
-		$this->db->delete('maestra1k',array('Id_estudiante'=>$Id_estudiante));
+		$id_estudiante=$_GET['ID_ESTUDIANTE'];
+		$this->db->delete('maestra',array('id_estudiante'=>$id_estudiante));
 
 	}
 	public function informacion_m()
 	{	
-		//$sql=$this->db->query('select Nombres,Apellidos from maestra1k');
+		//$sql=$this->db->query('select Nombres,Apellidos from maestra');
 		$Id_estudiante=$_GET['ID_ESTUDIANTE'];
-		$sql='select*from maestra1k where Id_estudiante=?';
+		$sql='select*from maestra where id_estudiante=?';
 		$sql=$this->db->query($sql,$Id_estudiante);
 		if($sql->num_rows()>0){
 			foreach ($sql->result() as $fila) :
